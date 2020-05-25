17cd3c8433e7650b9cf6475b56debfcdf49a9d8a
Joaquín Ormaechea <joaquinormaechea@gmail.com>
2020-05-24 23:45:55 -0300

    First working version

---
+++ tests/vtex-order-placer-test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// const assert = require('assert');
+
+// const sinon = require('sinon');
+
+// const {
+// 	VtexOrderPlacer,
+// 	VtexOrderPlacerError
+// } = require('../lib');
+
+describe('VtexOrderPlacer', () => {
+
+	// your tests here...
+
+});
